3a5bda7f5831ddadb23e373f5ee7309de1f54cb2
Ryan Muranaka <rmuranaka@hotmail.com>
2022-10-13 20:55:06 -0600

    Added startup for TcpClientManager and TcpServerManager

--- NetworkTester/Program.cs
+++ NetworkTester/Program.cs
@@ -1,4 +1,6 @@
 ﻿using NetworkTester.Log;
+using NetworkTester.Manager;
+using System.Net;
 using static NetworkTester.Manager.Data;
 
 namespace NetworkTester
@@ -6,11 +8,13 @@ namespace NetworkTester
 	public class Program
 	{
 		private const string INVALID_CALL = "Invalid call";
+		private const string TCP_SERVER_CALL = "TCP Server <listening port> [buffer size] [write rate]";
 		private const string TCP_CLIENT_CALL = "TCP Client <ip address> <port> [buffer size]";
 		private const string FOR_PROPER_USAGE = "For proper usage pass ?, -?, or /?";
 
 		public static void Main(string[] args)
 		{
+			CancellationTokenSource cancellationTokenSource = new();
 			try
 			{
 				if (args.Length == 0 || args.Length == 2)
@@ -36,21 +40,22 @@ namespace NetworkTester
 					return;
 				}
 
-				StartManager(protocol, application, args, logger);
+				StartManager(protocol, application, args, logger, cancellationTokenSource.Token);
 			}
 			finally
 			{
 				Console.WriteLine("Press any key to exit ...");
 				Console.ReadKey();
+
+				cancellationTokenSource.Cancel();
+				cancellationTokenSource.Dispose();
 			}
 		}
 
 		private static void ShowProperUsage(string requestUsage)
 		{
-			if (string.IsNullOrWhiteSpace(requestUsage) ||
-						!requestUsage.Equals("?", StringComparison.OrdinalIgnoreCase) &&
-						!requestUsage.Equals("-?", StringComparison.OrdinalIgnoreCase) &&
-						!requestUsage.Equals("/?", StringComparison.OrdinalIgnoreCase))
+			if (string.IsNullOrWhiteSpace(requestUsage) || !requestUsage.Equals("?", StringComparison.OrdinalIgnoreCase) &&
+				!requestUsage.Equals("-?", StringComparison.OrdinalIgnoreCase) && !requestUsage.Equals("/?", StringComparison.OrdinalIgnoreCase))
 			{
 				Console.WriteLine($"{INVALID_CALL}. {FOR_PROPER_USAGE}");
 				return;
@@ -58,7 +63,7 @@ namespace NetworkTester
 			Console.WriteLine();
 			Console.WriteLine("*********************************************************************************");
 			Console.WriteLine("    Usage: (<> required parameters, [] optional paramters)");
-			Console.WriteLine("    NetworkTester TCP Server <listening port> [buffer size] [write rate]");
+			Console.WriteLine($"    NetworkTester {TCP_SERVER_CALL}");
 			Console.WriteLine($"    NetworkTester {TCP_CLIENT_CALL}");
 			Console.WriteLine("    NetworkTester UDP Server <ip address> <port> [buffer size] [write rate]");
 			Console.WriteLine("    NetworkTester UDP Client <listening port> [buffer size]");
@@ -102,12 +107,12 @@ namespace NetworkTester
 			}
 		}
 
-		private static void StartManager(Protocol protocol, Application application, string[] args, ILogger logger)
+		private static void StartManager(Protocol protocol, Application application, string[] args, ILogger logger, CancellationToken cancellationToken)
 		{
 			switch (protocol)
 			{
 				case Protocol.TCP:
-					StartTcpManager(application, args, logger);
+					StartTcpManager(application, args, logger, cancellationToken);
 					break;
 				case Protocol.UDP:
 					break;
@@ -117,34 +122,70 @@ namespace NetworkTester
 			}
 		}
 
-		private static void StartTcpManager(Application application, string[] args, ILogger logger)
+		private static void StartTcpManager(Application application, string[] args, ILogger logger, CancellationToken cancellationToken)
 		{
 			switch (application)
 			{
 				case Application.Client:
-					if (args.Length < 3)
+					if (args.Length < 4)
 					{
 						logger.Log($"{INVALID_CALL}. {TCP_CLIENT_CALL}");
 						return;
 					}
-					if (string.IsNullOrWhiteSpace(args[2]) || !ushort.TryParse(args[2], out ushort listeningPort))
+					if (string.IsNullOrWhiteSpace(args[2]) || !IPAddress.TryParse(args[2], out IPAddress? ipAddress))
 					{
-						logger.Log($"TCP Client invalid listening port. {TCP_CLIENT_CALL}");
+						logger.Log($"TCP Client invalid IP address {args[2]}. {TCP_CLIENT_CALL}");
 						return;
 					}
-					int bufferSize;
-					if (args.Length == 4 )
+					if (string.IsNullOrWhiteSpace(args[3]) || !ushort.TryParse(args[3], out ushort port))
 					{
-						if (string.IsNullOrWhiteSpace(args[3]) ||
-							!int.TryParse(args[3], out bufferSize) ||
-							bufferSize < 1)
+						logger.Log($"TCP Client invalid port {args[3]}. {TCP_CLIENT_CALL}");
+						return;
+					}
+					int bufferSize = Data.DEFAULT_BUFFER_SIZE;
+					if (args.Length == 5)
+					{
+						if (string.IsNullOrWhiteSpace(args[4]) || !int.TryParse(args[4], out bufferSize) || bufferSize < 1)
 						{
-							logger.Log($"TCP client invalid buffer size. {TCP_CLIENT_CALL}");
+							logger.Log($"TCP Client invalid buffer size {args[4]}. {TCP_CLIENT_CALL}");
 							return;
 						}
 					}
+
+					_ = new TcpClientManager(ipAddress, port, logger, cancellationToken, bufferSize);
 					break;
 				case Application.Server:
+					// TCP Server <listening port> [buffer size] [write rate]
+					if (args.Length < 3)
+					{
+						logger.Log($"{INVALID_CALL}. {TCP_SERVER_CALL}");
+						return;
+					}
+					if (string.IsNullOrWhiteSpace(args[2]) || !ushort.TryParse(args[2], out ushort listeningPort))
+					{
+						logger.Log($"TCP Server invalid listening port {args[2]}. {TCP_SERVER_CALL}");
+						return;
+					}
+					bufferSize = Data.DEFAULT_BUFFER_SIZE;
+					if (args.Length >= 4)
+					{
+						if (string.IsNullOrWhiteSpace(args[3]) || !int.TryParse(args[3], out bufferSize) || bufferSize < 1)
+						{
+							logger.Log($"TCP Server invalid buffer size {args[3]}. {TCP_SERVER_CALL}");
+							return;
+						}
+					}
+					byte writeRate = Data.DEFAULT_WRITE_RATE;
+					if (args.Length == 5)
+					{
+						if (string.IsNullOrWhiteSpace(args[4]) || !byte.TryParse(args[4], out writeRate) || writeRate < 1)
+						{
+							logger.Log($"TCP Server invalid write rate {args[4]}. {TCP_SERVER_CALL}");
+							return;
+						}
+					}
+
+					_ = new TcpServerManager(listeningPort, logger, cancellationToken, bufferSize, writeRate);
 					break;
 				default:
 					logger.Log($"Invalid application {application}");
